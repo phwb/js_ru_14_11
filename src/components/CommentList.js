4c5ac3d205ff4b9d075ddf1b78a1083be52f718f
yvv <yvv@ugraweb.ru>
2016-11-21 19:27:46 +0500

    add comment list

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Comment from './Comment'
 
-const CommentList = props => {
-  const { comments } = props
-  const commentItems = comments.map(item => <Comment key={item.id} item={item} />)
+// const CommentList = props => {
+//   const { comments } = props
+//   const commentItems = comments.map(item => <Comment key={item.id} item={item} />)
+//
+//   return (
+//     <ul>{commentItems}</ul>
+//   )
+// }
 
-  return (
-    <ul>{commentItems}</ul>
-  )
+class CommentList extends Component {
+  constructor () {
+    super()
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  render () {
+    const { isOpen } = this.state
+    const { comments } = this.props
+    let body = <i style={{color: '#ccc'}}>Еще никто не комментировал</i>
+
+    if (!!comments) {
+      const link = <a href="#" onClick={this.openComment}>{isOpen ? 'Закрыть' : 'Открыть'} комментарии</a>
+      const commentItems = comments.map(item => <Comment key={item.id} item={item} />)
+      const commentList = isOpen
+        ? <ul>{commentItems}</ul>
+        : null
+
+      body = (
+        <div>
+          {link}
+          {commentList}
+        </div>
+      )
+    }
+
+    return body
+  }
+
+  openComment = e => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+    e.preventDefault()
+  }
 }
 
 export default CommentList
