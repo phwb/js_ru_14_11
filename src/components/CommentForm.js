d6f98680f429c34d03c3cd9d0013aa9fcdb47cef
yvv <yvv@ugraweb.ru>
2016-11-25 13:56:29 +0500

    HT3.2: добавил стейт к форме

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
 
 export default class CommentForm extends Component {
+  state = {
+    user: '',
+    text: ''
+  }
+
+  change = key => e => {
+    this.setState({
+      [key]: e.target.value
+    })
+  }
+
   render () {
+    console.log(this.state)
+
     return (
       <table style={{marginTop: '10px'}}>
         <tbody>
           <tr>
             <td>User:</td>
-            <td><input type="text"/></td>
+            <td><input type="text" onChange={this.change('user')} defaultValue={this.state.user}/></td>
           </tr>
           <tr>
             <td>Text:</td>
-            <td><textarea /></td>
+            <td><textarea onChange={this.change('text')} defaultValue={this.state.text} /></td>
           </tr>
           <tr>
             <td />
