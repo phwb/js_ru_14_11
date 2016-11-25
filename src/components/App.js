dda0cce9cc3e0fe5f2c6afe906a11c58fcf7b0a4
yvv <yvv@ugraweb.ru>
2016-11-25 11:30:19 +0500

    HT3.1: добавить календарь(https://github.com/gpbl/react-day-picker) с возможностью выбора промежутка дат. Показывать этот промежуток текстом

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import ArticleList from './ArticleList'
 import Select from 'react-select'
 import Chart from './Chart'
+import SelectDayRange from './SelectDayRange'
 import 'react-select/dist/react-select.css'
 
 class App extends Component {
@@ -23,6 +24,7 @@ class App extends Component {
                 <Chart />
                 <ArticleList articles={this.props.articles} />
                 <Select options = {options} value = {this.state.selected} onChange = {this.handleChange} multi = {true}/>
+                <SelectDayRange />
             </div>
         )
     }
